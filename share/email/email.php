337936ef3395eeb222570b355bd2f44bf53ffb38
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2022-10-10 22:12:41 +0300

    Fixed

--- share/email/email.php
+++ share/email/email.php
@@ -1,36 +1,36 @@
 <?php
 // PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer; 
+use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 function sendNotificationEmail($toEmail,$subject,$messageBody)
 {
- 
+
 
 
 //--------------------------------
 $smtpServer   = "outlook.live.com";
 $smtpPort     = 587;
 $smtpUser     = "AKHDB@outlook.com";
-$smtpPassword = 'Backup@2013$$ali';
+$smtpPassword = '******';
 //------------------------------------
 
 
 
-// Base files 
+// Base files
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 // create object of PHPMailer class with boolean parameter which sets/unsets exception.
-$mail = new PHPMailer(true);                              
+$mail = new PHPMailer(true);
 try {
-    $mail->isSMTP(); // using SMTP protocol                                     
-    $mail->Host = $smtpServer; // SMTP host as gmail 
-    $mail->SMTPAuth = true;  // enable smtp authentication                             
-    $mail->Username = $smtpUser  ;  // sender gmail host              
-    $mail->Password = $smtpPassword; // sender gmail host password                          
-    $mail->SMTPSecure = 'tls';  // for encrypted connection                           
-    $mail->Port = 587;   // port for SMTP     
+    $mail->isSMTP(); // using SMTP protocol
+    $mail->Host = $smtpServer; // SMTP host as gmail
+    $mail->SMTPAuth = true;  // enable smtp authentication
+    $mail->Username = $smtpUser  ;  // sender gmail host
+    $mail->Password = $smtpPassword; // sender gmail host password
+    $mail->SMTPSecure = 'tls';  // for encrypted connection
+    $mail->Port = 587;   // port for SMTP
 
     $mail->setFrom($smtpUser, "FleetM System"); // sender's email and name
     $mail->addAddress($toEmail, "Receiver");  // receiver's email and name
